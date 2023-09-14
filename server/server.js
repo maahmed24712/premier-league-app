d0b7ba25474fdcffcf09ab656f4243914e57f50c
Mohamed Ali Ahmed <139277650+maahmed24712@users.noreply.github.com>
2023-09-14 15:32:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const Teams = require('./models/Teams');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: [""],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(cors()); // Invoke the cors middleware
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/PremierLeaguePlayers', {
@@ -31,6 +27,7 @@ app.get('/api/teams', async (req, res) => {
     console.log(club);
 
     const foundTeams = await Teams.find({ "teams.name": club });
+    console.log(foundTeams)
 
     if (foundTeams.length === 0) {
       return res.status(404).json({ error: 'Team not found' });
@@ -107,3 +104,4 @@ app.get('/api/players/country/:country', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
