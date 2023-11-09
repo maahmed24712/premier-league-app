e0f46b44d485eb2eca22c42a910183edeb51c3c2
maaahmed <ec20911@qmul.ac.uk>
2023-11-09 16:50:22 +0000

    updating cors

--- server/server.js
+++ server/server.js
@@ -9,9 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const connect = process.env.MONGODB_URI;
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:3000', 'https://premier-league-app-backend.onrender.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 mongoose.connect(connect, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
